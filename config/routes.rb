3702e31b787669d5653447c9be874c95a23f634f
Thiago Rose <thiago.senhorinha@gmail.com>
2016-04-21 23:44:10 -0300

    minor fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'sustainability_level_checks#index'
   resources :sustainability_level_checks, :only => [:index]
-  post '/check' => 'sustainability_level_checks#check'
+  get 'check' => 'sustainability_level_checks#check'
   get '/fetch_last_check' => 'sustainability_level_checks#fetch_last_check'
 
 end
